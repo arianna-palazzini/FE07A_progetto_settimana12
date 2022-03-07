e990bca32b7b20ef8c52e4b0086e3447970c6fee
ari_plz <arianna.palazzini@gmail.com>
2022-03-07 22:51:41 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <app-navbar></app-navbar>
+  <div id="sfondo">
+    <router-outlet></router-outlet>
+  </div>
+  `,
+  styles: [`
+
+  #sfondo {
+      background-image: url("../assets/sfondo.jpg");
+      background-position: center;
+      text-align: center;
+      height: 500vh;
+    }
+  `]
+})
+export class AppComponent {
+  constructor(){
+
+  }
+}
